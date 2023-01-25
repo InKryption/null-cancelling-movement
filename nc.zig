a29e41ce23277500f61306873acaab7a55a2f112
InKryption <inkryption07@gmail.com>
2023-01-25 04:46:18 +0100

    Make things super simple; can just be a reference

--- nc.zig
+++ nc.zig
@@ -1,29 +1,24 @@
 const std = @import("std");
+const assert = std.debug.assert;
 
 /// Monad-like enum representing the transformation of two boolean inputs, into a single
 /// output with three possible values (as opposed to the original 4 possible permutations).
-pub const NCState = enum(u4) {
-    // zig fmt: off
-    off          = 0x0,
-    a_exclusive  = 0x1,
-    b_exclusive  = 0x2,
-    a_transition = 0x4,
-    b_transition = 0x8,
-    // zig fmt: on
+pub const NCState = enum(u3) {
+    off,
+    a_exclusive,
+    a_transition,
+    b_exclusive,
+    b_transition,
 
-    pub inline fn init() NCState {
-        return .off;
-    }
-
-    pub const Bias = Decision;
-    pub const Decision = enum(u2) { none, a, b };
     pub const Input = enum(u2) {
-        none = @enumToInt(NCState.off),
-        only_a = @enumToInt(NCState.a_exclusive),
-        only_b = @enumToInt(NCState.b_exclusive),
-        both_ab = 3,
+        none,
+        only_a,
+        only_b,
+        both,
     };
-    pub fn DecisionCustomValues(comptime T: type) type {
+
+    pub const Decision = enum(u2) { none, a, b };
+    pub fn DecisionValMap(comptime T: type) type {
         return struct {
             none: T,
             a: T,
@@ -31,123 +26,35 @@ pub const NCState = enum(u4) {
         };
     }
 
+    pub inline fn init() NCState {
+        return .off;
+    }
+
     /// Use to initialise an input state.
     pub inline fn input(a: bool, b: bool) Input {
-        const a_int: u2 = @boolToInt(a);
-        const b_int: u2 = @as(u2, @boolToInt(b)) << 1;
-        return @intToEnum(Input, a_int | b_int);
+        const Bits = packed struct(u2) { a: bool, b: bool };
+        const bits = @bitCast(u2, Bits{ .a = a, .b = b });
+        return @intToEnum(Input, bits);
+    }
+    comptime {
+        // zig fmt: off
+        assert(input(false, false) == .none);
+        assert(input(true,  false) == .only_a);
+        assert(input(false, true ) == .only_b);
+        assert(input(true,  true ) == .both);
+        // zig fmt: on
     }
 
     /// Determines the next state based on the input.
-    pub inline fn decide(ncs: NCState, in: Input) NCState {
-        return ncs.decideWithBias(in, .none);
-    }
-
-    /// Returns the decision determined by the last input.
-    pub inline fn decision(ncs: NCState) Decision {
-        return ncs.decisionCustom(Decision, .{
-            .a = .a,
-            .b = .b,
-            .none = .none,
-        });
-    }
-
-    // configurable functions
-
-    /// Like `decide`, except a bias can be specified for what decision
-    /// to output in the case of simultaneous input following no input.
-    pub fn decideWithBias(
-        ncs: NCState,
-        in: Input,
-        comptime bias: Bias,
-    ) NCState {
-        const is_transition_value = in == .both_ab;
-
-        const exclusive_mask = @boolToInt(!is_transition_value) * @enumToInt(in);
-        const transition_mask = @boolToInt(is_transition_value) * blk: {
-            const bias_for_a = (comptime bias == .a) and ncs == .off;
-            const bias_for_b = (comptime bias == .b) and ncs == .off;
-
-            const enable_a_mask = bias_for_a or ncs.isEither(.b_exclusive, .a_transition);
-            const enable_b_mask = bias_for_b or ncs.isEither(.a_exclusive, .b_transition);
-
-            const trans_a_mask = @enumToInt(NCState.a_transition) * @boolToInt(enable_a_mask);
-            const trans_b_mask = @enumToInt(NCState.b_transition) * @boolToInt(enable_b_mask);
-            break :blk trans_a_mask | trans_b_mask;
-        };
-
-        return @intToEnum(NCState, exclusive_mask | transition_mask);
-    }
-
-    /// Like `decision`, except the possible outputs are configurable.
-    pub fn decisionCustom(
-        ncs: NCState,
-        comptime T: type,
-        comptime values: DecisionCustomValues(T),
-    ) T {
-        switch (@typeInfo(T)) {
-            .Int, .Float, .Bool, .Enum, .ErrorSet => {},
-            else => {
-                const arr = [_]T{ values.none, values.a, values.b };
-                const index = ncs.decisionCustom(std.math.IntFittingRange(0, arr.len), .{
-                    .none = 0,
-                    .a = 1,
-                    .b = 2,
-                });
-                return arr[index];
-            },
-        }
-
-        const a_bits = comptime toInt(values.a);
-        const b_bits = comptime toInt(values.b);
-        const none_bits = comptime toInt(values.none);
-
-        const maybe_a = a_bits * @boolToInt(ncs.isEither(.a_exclusive, .a_transition));
-        const maybe_b = b_bits * @boolToInt(ncs.isEither(.b_exclusive, .b_transition));
-        const maybe_none = none_bits * @boolToInt(ncs == .off);
-        return fromInt(T, maybe_a | maybe_b | maybe_none);
-    }
-
-    // normal function behaviour models
-
-    /// Function whose behaviour the public `decision` function is intended to model
-    inline fn decisionModel(ncs: NCState) Decision {
-        return ncs.decisionCustomModel(Decision, .{
-            .a = .a,
-            .b = .b,
-            .none = .none,
-        });
-    }
-
-    /// Function whose behaviour the public `decide` function is intended to model
-    inline fn decideModel(ncs: NCState, in: Input) NCState {
-        return ncs.decideWithBiasModel(in, .none);
-    }
-
-    // configurable function behaviour models
-
-    fn decisionCustomModel(
-        ncs: NCState,
-        comptime T: type,
-        comptime values: DecisionCustomValues(T),
-    ) T {
-        return switch (ncs) {
-            .off => values.none,
-            .a_exclusive, .a_transition => values.a,
-            .b_exclusive, .b_transition => values.b,
-        };
-    }
-
-    fn decideWithBiasModel(
-        ncs: NCState,
-        in: Input,
-        comptime bias: Bias,
-    ) NCState {
+    /// A bias can be specified for what decision
+    /// to output in the case of simultaneous input
+    /// following no input.
+    pub fn decide(ncs: NCState, in: Input, comptime bias: Decision) NCState {
         return switch (in) {
             .none => .off,
             .only_a => .a_exclusive,
             .only_b => .b_exclusive,
-            .both_ab => switch (ncs) {
+            .both => switch (ncs) {
                 .a_transition, .b_exclusive => .a_transition,
                 .a_exclusive, .b_transition => .b_transition,
                 .off => comptime switch (bias) {
@@ -159,81 +66,40 @@ pub const NCState = enum(u4) {
         };
     }
 
-    // helper functions
+    /// Same as `decide`, but assigns the resulting state
+    /// to the original location.
+    pub fn decideInPlace(ncs: *NCState, in: Input, comptime bias: Decision) void {
+        ncs.* = @call(.always_inline, decide, .{ ncs.*, in, bias });
+    }
 
-    inline fn isEither(ncs: NCState, x: NCState, y: NCState) bool {
-        return 0 != @enumToInt(ncs) & (@enumToInt(x) | @enumToInt(y));
+    /// Returns the decision determined by the last input.
+    pub fn decision(ncs: NCState) Decision {
+        return @call(.always_inline, decisionAs, .{ ncs, Decision, .{
+            .a = .a,
+            .b = .b,
+            .none = .none,
+        } });
+    }
+
+    /// Like `decision`, except the possible outputs are configurable.
+    pub fn decisionAs(
+        ncs: NCState,
+        comptime T: type,
+        comptime values: DecisionValMap(T),
+    ) T {
+        return switch (ncs) {
+            .off => values.none,
+            .a_exclusive, .a_transition => values.a,
+            .b_exclusive, .b_transition => values.b,
+        };
     }
 };
 
-inline fn toInt(value: anytype) std.meta.Int(.unsigned, @bitSizeOf(@TypeOf(value))) {
-    const T = @TypeOf(value);
-    const Int = std.meta.Int(.unsigned, @bitSizeOf(T));
-    return @bitCast(Int, switch (@typeInfo(T)) {
-        .ErrorSet => @errorToInt(value),
-        .Bool => @boolToInt(value),
-        .Enum => @enumToInt(value),
-        else => value,
-    });
-}
-inline fn fromInt(comptime T: type, value: std.meta.Int(.unsigned, @bitSizeOf(T))) T {
-    return switch (@typeInfo(T)) {
-        .ErrorSet => @errSetCast(T, @intToError(value)),
-        .Bool => @bitCast(T, value),
-        .Enum => |enumeration| @intToEnum(T, @bitCast(enumeration.tag_type, value)),
-        else => @bitCast(T, value),
-    };
-}
-
-comptime {
-    var err_msg: []const u8 = "";
-    defer if (err_msg.len != 0) @compileError(err_msg);
-
-    const ncs_values = std.enums.values(NCState);
-    const input_values = std.enums.values(NCState.Input);
-    const bias_values = std.enums.values(NCState.Bias);
-
-    for (ncs_values) |ncs| {
-        const expected = ncs.decisionModel();
-        const actual = ncs.decision();
-        if (actual != expected) err_msg = err_msg ++ std.fmt.comptimePrint(
-            "Expected for `{}.decision(.{s})` to evaluate to `{}`, instead evaluated to `{}`.\n",
-            .{ @TypeOf(ncs), @tagName(ncs), expected, actual },
-        );
-    }
-
-    for (ncs_values) |ncs| {
-        for (input_values) |in| {
-            {
-                const expected = ncs.decideModel(in);
-                const actual = ncs.decide(in);
-
-                if (actual != expected) err_msg = err_msg ++ std.fmt.comptimePrint(
-                    "Expected for `{}.decide(.{s}, {})` to evaluate to `{}`, instead evaluated to `{}`.\n",
-                    .{ @TypeOf(ncs), @tagName(ncs), in, expected, actual },
-                );
-            }
-
-            for (bias_values) |bias| {
-                const expected = ncs.decideWithBiasModel(in, bias);
-                const actual = ncs.decideWithBias(in, bias);
-
-                if (actual != expected) err_msg = err_msg ++ std.fmt.comptimePrint(
-                    "Expected for `{}.decideWithBias(.{s}, {})` to evaluate to `{}`, instead evaluated to `{}`.\n",
-                    .{ @TypeOf(ncs), @tagName(ncs), in, expected, actual },
-                );
-            }
-        }
-    }
-}
-
 fn expectDecisionInPlace(ncs: *NCState, input: NCState.Input, expected: NCState.Decision) !void {
-    ncs.* = ncs.decide(input);
-    const actual = ncs.decision();
-    return std.testing.expectEqual(expected, actual);
+    return expectDecisionWithBiasInPlace(ncs, input, .none, expected);
 }
-fn expectDecisionWithBiasInPlace(ncs: *NCState, input: NCState.Input, comptime bias: NCState.Bias, expected: NCState.Decision) !void {
-    ncs.* = ncs.decideWithBias(input, bias);
+fn expectDecisionWithBiasInPlace(ncs: *NCState, input: NCState.Input, comptime bias: NCState.Decision, expected: NCState.Decision) !void {
+    ncs.decideInPlace(input, bias);
     const actual = ncs.decision();
     return std.testing.expectEqual(expected, actual);
 }
@@ -242,10 +108,10 @@ test "Example Usage" {
     var ncs = NCState.init();
     try std.testing.expectEqual(NCState.Decision.none, ncs.decision());
 
-    ncs = ncs.decide(NCState.input(true, false));
+    ncs.decideInPlace(NCState.input(true, false), .none);
     try std.testing.expectEqual(NCState.Decision.a, ncs.decision());
 
-    ncs = ncs.decide(NCState.input(true, true));
+    ncs.decideInPlace(NCState.input(true, true), .none);
     try std.testing.expectEqual(NCState.Decision.b, ncs.decision());
 }
 
