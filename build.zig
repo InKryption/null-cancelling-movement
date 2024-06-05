4dfe0e804cc9e4b6eac121ed324d6f045e7935df
InKryption <59504965+InKryption@users.noreply.github.com>
2024-06-05 04:58:27 +0000

    Update build.zig

--- build.zig
+++ build.zig
@@ -3,6 +3,6 @@ const Build = std.Build;
 
 pub fn build(b: *Build) void {
     _ = b.addModule("ncs", Build.CreateModuleOptions{
-        .source_file = b.path("input-lock.zig"),
+        .root_source_file = b.path("input-lock.zig"),
     });
 }
