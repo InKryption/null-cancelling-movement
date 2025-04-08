0adc70ed1da8896cf1d1a6fd7c05497c660ef913
InKryption <inkryption07@gmail.com>
2025-04-08 05:38:17 +0200

    Greatly simplify code, become ref implementation

--- input-lock.zig
+++ input-lock.zig
@@ -1,62 +1,38 @@
 const std = @import("std");
 const assert = std.debug.assert;
 
-pub const Input = packed struct(u2) {
-    a: bool,
-    b: bool,
+pub const Input = enum(u2) { none, a, b, ab };
+pub const Decision = enum(u2) { none, a, b };
+pub const Lock = enum(u3) {
+    inactive,
+    stalemate,
 
-    inline fn permutation(input: Input) Permutation {
-        return @enumFromInt(@as(u2, @bitCast(input)));
-    }
-    const Permutation = enum(u2) {
-        ab = @bitCast(Input{ .a = true, .b = true }),
-        a_ = @bitCast(Input{ .a = true, .b = false }),
-        _b = @bitCast(Input{ .a = false, .b = true }),
-        __ = @bitCast(Input{ .a = false, .b = false }),
-    };
-};
+    a_exclusive,
+    a_contested,
 
-pub const InputLock = enum(u3) {
-    inactive = @bitCast(State{ .held_by = .none, .contested = false }),
-    stalemate = @bitCast(State{ .held_by = .none, .contested = true }),
+    b_exclusive,
+    b_contested,
 
-    a_exclusive = @bitCast(State{ .held_by = .a, .contested = false }),
-    a_contested = @bitCast(State{ .held_by = .a, .contested = true }),
+    pub fn decision(inlock: Lock) Decision {
+        return switch (inlock) {
+            .inactive => .none,
+            .stalemate => .none,
 
-    b_exclusive = @bitCast(State{ .held_by = .b, .contested = false }),
-    b_contested = @bitCast(State{ .held_by = .b, .contested = true }),
+            .a_exclusive => .a,
+            .a_contested => .a,
 
-    pub const Decision = enum(u2) { none, a, b };
-
-    pub const init: InputLock = .inactive;
-
-    pub inline fn decision(inlock: InputLock) Decision {
-        const inlock_state: State = @bitCast(@intFromEnum(inlock));
-        return inlock_state.held_by;
-    }
-    comptime {
-        for (@typeInfo(InputLock).@"enum".fields) |field| {
-            const tag = @field(InputLock, field.name);
-            switch (tag) {
-                .inactive => assert(decision(tag) == .none),
-                .stalemate => assert(decision(tag) == .none),
-
-                .a_exclusive => assert(decision(tag) == .a),
-                .a_contested => assert(decision(tag) == .a),
-
-                .b_exclusive => assert(decision(tag) == .b),
-                .b_contested => assert(decision(tag) == .b),
-            }
-        }
+            .b_exclusive => .b,
+            .b_contested => .b,
+        };
     }
 
-    pub fn updateCopy(inlock: InputLock, inputs: Input, comptime bias: Decision) InputLock {
-        return switch (inputs.permutation()) {
-            .__ => .inactive,
-            .a_ => .a_exclusive,
-            ._b => .b_exclusive,
+    pub fn update(inlock: Lock, inputs: Input, bias: Decision) Lock {
+        return switch (inputs) {
+            .none => .inactive,
+            .a => .a_exclusive,
+            .b => .b_exclusive,
             .ab => switch (inlock) {
-                .inactive, .stalemate => comptime switch (bias) {
+                .inactive, .stalemate => switch (bias) {
                     .none => .stalemate,
                     .a => .a_contested,
                     .b => .b_contested,
@@ -66,76 +42,36 @@ pub const InputLock = enum(u3) {
             },
         };
     }
-
-    pub fn updateInPlace(inlock: *InputLock, inputs: Input, comptime bias: Decision) void {
-        inlock.* = @call(.always_inline, updateCopy, .{ inlock.*, inputs, bias });
-    }
-
-    pub fn DecisionAsVals(comptime T: type) type {
-        return struct { none: T, a: T, b: T };
-    }
-    pub fn decisionAs(
-        inlock: InputLock,
-        comptime T: type,
-        comptime values: DecisionAsVals(T),
-    ) T {
-        return switch (inlock.decision()) {
-            inline else => |tag| @field(values, @tagName(tag)),
-        };
-    }
-
-    const State = packed struct(u3) {
-        held_by: Decision = .none,
-        contested: bool = false,
-    };
 };
 
-fn expectDecisionWithBias(
-    il: *InputLock,
-    inputs: Input,
-    comptime bias: InputLock.Decision,
-    expected: InputLock.Decision,
-) !void {
-    il.updateInPlace(inputs, bias);
-    const actual = il.decision();
-    return std.testing.expectEqual(expected, actual);
-}
-fn expectDecision(
-    il: *InputLock,
-    inputs: Input,
-    expected: InputLock.Decision,
-) !void {
-    return expectDecisionWithBias(il, inputs, .none, expected);
-}
-
-test InputLock {
-    for (comptime std.enums.values(InputLock)) |state| {
+test Lock {
+    for (comptime std.enums.values(Lock)) |state| {
         inline for (.{ .none, .a, .b }) |bias| {
-            try std.testing.expectEqual(InputLock.inactive, InputLock.updateCopy(state, .{ .a = false, .b = false }, bias));
-            try std.testing.expectEqual(InputLock.a_exclusive, InputLock.updateCopy(state, .{ .a = true, .b = false }, bias));
-            try std.testing.expectEqual(InputLock.b_exclusive, InputLock.updateCopy(state, .{ .a = false, .b = true }, bias));
+            try std.testing.expectEqual(Lock.inactive, Lock.update(state, .none, bias));
+            try std.testing.expectEqual(Lock.a_exclusive, Lock.update(state, .a, bias));
+            try std.testing.expectEqual(Lock.b_exclusive, Lock.update(state, .b, bias));
         }
     }
-    try std.testing.expectEqual(InputLock.stalemate, InputLock.updateCopy(.inactive, .{ .a = true, .b = true }, .none));
-    try std.testing.expectEqual(InputLock.stalemate, InputLock.updateCopy(.stalemate, .{ .a = true, .b = true }, .none));
-    try std.testing.expectEqual(InputLock.b_contested, InputLock.updateCopy(.a_exclusive, .{ .a = true, .b = true }, .none));
-    try std.testing.expectEqual(InputLock.a_contested, InputLock.updateCopy(.b_exclusive, .{ .a = true, .b = true }, .none));
+    try std.testing.expectEqual(Lock.stalemate, Lock.update(.inactive, .ab, .none));
+    try std.testing.expectEqual(Lock.stalemate, Lock.update(.stalemate, .ab, .none));
+    try std.testing.expectEqual(Lock.b_contested, Lock.update(.a_exclusive, .ab, .none));
+    try std.testing.expectEqual(Lock.a_contested, Lock.update(.b_exclusive, .ab, .none));
 
-    try std.testing.expectEqual(InputLock.a_contested, InputLock.updateCopy(.inactive, .{ .a = true, .b = true }, .a));
-    try std.testing.expectEqual(InputLock.a_contested, InputLock.updateCopy(.stalemate, .{ .a = true, .b = true }, .a));
-    try std.testing.expectEqual(InputLock.b_contested, InputLock.updateCopy(.a_exclusive, .{ .a = true, .b = true }, .a));
-    try std.testing.expectEqual(InputLock.a_contested, InputLock.updateCopy(.b_exclusive, .{ .a = true, .b = true }, .a));
+    try std.testing.expectEqual(Lock.a_contested, Lock.update(.inactive, .ab, .a));
+    try std.testing.expectEqual(Lock.a_contested, Lock.update(.stalemate, .ab, .a));
+    try std.testing.expectEqual(Lock.b_contested, Lock.update(.a_exclusive, .ab, .a));
+    try std.testing.expectEqual(Lock.a_contested, Lock.update(.b_exclusive, .ab, .a));
 
-    try std.testing.expectEqual(InputLock.b_contested, InputLock.updateCopy(.inactive, .{ .a = true, .b = true }, .b));
-    try std.testing.expectEqual(InputLock.b_contested, InputLock.updateCopy(.stalemate, .{ .a = true, .b = true }, .b));
-    try std.testing.expectEqual(InputLock.b_contested, InputLock.updateCopy(.a_exclusive, .{ .a = true, .b = true }, .b));
-    try std.testing.expectEqual(InputLock.a_contested, InputLock.updateCopy(.b_exclusive, .{ .a = true, .b = true }, .b));
+    try std.testing.expectEqual(Lock.b_contested, Lock.update(.inactive, .ab, .b));
+    try std.testing.expectEqual(Lock.b_contested, Lock.update(.stalemate, .ab, .b));
+    try std.testing.expectEqual(Lock.b_contested, Lock.update(.a_exclusive, .ab, .b));
+    try std.testing.expectEqual(Lock.a_contested, Lock.update(.b_exclusive, .ab, .b));
 
-    var ncs = InputLock.init;
-    const _______ = comptime Input{ .a = false, .b = false };
-    const @"<== " = comptime Input{ .a = true, .b = false };
-    const @" ==>" = comptime Input{ .a = false, .b = true };
-    const @"<==>" = comptime Input{ .a = true, .b = true };
+    var ncs: Lock = .inactive;
+    const _______: Input = .none;
+    const @"<== ": Input = .a;
+    const @" ==>": Input = .b;
+    const @"<==>": Input = .ab;
 
     try expectDecision(&ncs, _______, .none);
     try expectDecision(&ncs, _______, .none);
@@ -188,4 +124,22 @@ test InputLock {
     try expectDecision(&ncs, @"<==>", .a);
     try expectDecision(&ncs, @"<== ", .a);
     try expectDecision(&ncs, _______, .none);
+}
+
+fn expectDecisionWithBias(
+    il: *Lock,
+    inputs: Input,
+    comptime bias: Decision,
+    expected: Decision,
+) !void {
+    il.* = il.update(inputs, bias);
+    const actual = il.decision();
+    return std.testing.expectEqual(expected, actual);
+}
+fn expectDecision(
+    il: *Lock,
+    inputs: Input,
+    expected: Decision,
+) !void {
+    return expectDecisionWithBias(il, inputs, .none, expected);
 }
